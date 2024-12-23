0da836a9dcdc29a9d1a3eccc25521520dd5d86d0
shiitake <pegtar377512@gmail.com>
2024-12-23 13:01:06 +0900

    chore: modify directory structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
+      <h1 className="text-4xl font-bold">About</h1>
+      <p className="text-lg">
+        This is a portfolio site for a fictional web developer.
+      </p>
+    </div>
+  );
+}
---
+++ src/lib/metadata.ts
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+  keywords: "",
+  authors: [{ name: "", url: "" }],
+  generator: "Next.js",
+  applicationName: "",
+  openGraph: {
+    title: "",
+    description: "",
+    type: "website",
+    url: "",
+    siteName: "",
+    locale: "ja_JP",
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 630,
+        alt: "",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: "",
+    description: "",
+    images: [""],
+    creator: "",
+  },
+};
