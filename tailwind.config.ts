cea7ba9884f808697f5f52417f564eaa175ba6bc
shiitake <pegtar377512@gmail.com>
2024-12-27 18:35:16 +0900

    chore: update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ export default {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        highlight: "hsl(var(--highlight))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
