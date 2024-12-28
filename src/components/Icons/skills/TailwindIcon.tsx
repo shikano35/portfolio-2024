5fbdd16b6e5592d04fbcf8a2e8b0804d2412eb3c
shiitake <pegtar377512@gmail.com>
2024-12-28 21:26:12 +0900

    refactor: rename TailwindCSSIcon.tsx to TailwindIcon.tsx

--- src/components/Icons/skills/TailwindIcon.tsx
+++ src/components/Icons/skills/TailwindIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function TailwindCSSIcon(props: React.SVGProps<SVGSVGElement>) {
+export function TailwindIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
