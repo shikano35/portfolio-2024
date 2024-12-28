c40ed820be898fa5e78b626e6f00e33b45fb36e6
shiitake <pegtar377512@gmail.com>
2024-12-29 01:03:45 +0900

    feat: Add FadeIn component for animation effects

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,9 +1,45 @@
+import { FadeIn, FadeInWithStagger } from "@/components/FadeIn";
 import React from "react";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-    </div>
+    <FadeInWithStagger>
+      <FadeIn>Item 1</FadeIn>
+      <FadeIn>Item 2</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+      <FadeIn>Item 3</FadeIn>
+    </FadeInWithStagger>
   );
 }
