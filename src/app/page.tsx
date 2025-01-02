8b6dfa2890164cd17b8fdc88cbdc8db251dba8f9
shiitake <pegtar377512@gmail.com>
2025-01-02 23:02:11 +0900

    feat: add Heading, FadeIn, and Container components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { MyIcon } from "@/components/Icons/MyIcon";
 import { FadeIn } from "@/components/FadeIn";
+import { Heading } from "@/components/Heading";
+import { Container } from "@/components/Container";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden px-16 py-24 md:px-32 md:py-32 md:pb-32 relative">
-      <div className="mx-auto max-w-5xl md:grid md:grid-cols-12 md:gap-x-8 md:gap-y-10">
-        <div className="relative z-10 mx-auto max-w-2xl md:col-span-6 md:pt-6">
+    <Container className="overflow-hidden py-24 md:py-32 md:pb-32">
+      <div className="md:grid md:grid-cols-12 md:gap-y-10 md:pt-6">
+        <div className="relative z-10 md:col-span-6">
+          <Heading className="md:text-start">Profile</Heading>
           <FadeIn>
-            <h1 className="text-4xl md:text-5xl font-semibold text-primary text-center md:text-start">
-              Profile
-            </h1>
+            <p className="text-center md:text-start mt-16 text-muted-foreground text-sm leading-8 md:text-base md:leading-10">
+              深く考えながら、少しずつ学びを積み重ねるエンジニアです。
+            </p>
           </FadeIn>
           <FadeIn>
-            <p className="text-start sm:text-center md:text-start mt-16 text-muted-foreground text-sm leading-8 md:text-base md:leading-10">
-              深く考えながら、少しずつ学びを積み重ねるエンジニアです。
-              <br />
+            <p className="text-center md:text-start text-muted-foreground text-sm leading-8 md:text-base md:leading-10">
               私の作った作品が、少しでも誰かの生活に寄り添えたら嬉しいです。
             </p>
           </FadeIn>
         </div>
         <div className="relative z-0 md:col-span-5 md:flex md:items-center">
-          <div className="absolute top-1/2 transform -translate-y-1/2 -translate-x-60 sm:-translate-x-32 md:-translate-x-72 lg:-translate-x-40 opacity-30 md:opacity-50">
+          <div className="absolute top-1/2 transform -translate-y-1/2 -translate-x-60 sm:-translate-x-32 md:-translate-x-64 lg:-translate-x-40 opacity-30 md:opacity-50">
             <MyIcon />
           </div>
         </div>
@@ -36,6 +37,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
