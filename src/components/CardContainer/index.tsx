b3ff9245647fdb56b52586b7ffcc6c3d85a13120
shiitake <pegtar377512@gmail.com>
2025-01-03 23:28:08 +0900

    feat: create CardContainer component

---
+++ src/components/CardContainer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function CardContainer({ children }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {children}
+    </div>
+  );
+}
