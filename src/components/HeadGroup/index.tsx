eae1fe33c211c070fd0acf6f97d5fea2f2627452
shiitake <pegtar377512@gmail.com>
2025-01-05 01:10:27 +0900

    feat: add HeadGroup component

---
+++ src/components/HeadGroup/index.tsx
@@ -0,0 +1,18 @@
+import React, { ElementType, ReactNode, ComponentPropsWithoutRef } from "react";
+import { cn } from "@/lib/utils";
+
+type HeadGroupProps<T extends ElementType = "div"> = {
+  as?: T;
+  children: ReactNode;
+  className?: string;
+};
+
+export function HeadGroup<T extends ElementType = "div">({
+  as,
+  children,
+  className,
+}: HeadGroupProps<T> & ComponentPropsWithoutRef<T>) {
+  const Component = as || "div";
+
+  return <Component className={cn("mt-40", className)}>{children}</Component>;
+}
