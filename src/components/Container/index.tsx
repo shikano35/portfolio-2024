f0905a585140df263d2dc7f88537938a1ab15eac
shiitake <pegtar377512@gmail.com>
2025-01-04 21:45:31 +0900

    refactor: update props for Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -11,15 +11,11 @@ export function Container<T extends ElementType = "div">({
   as,
   children,
   className,
-  ...rest
 }: ContainerProps<T> & ComponentPropsWithoutRef<T>) {
   const Component = as || "div";
 
   return (
-    <Component
-      className={cn("mx-auto max-w-5xl px-6 lg:px-12", className)}
-      {...rest}
-    >
+    <Component className={cn("mx-auto max-w-5xl px-6 lg:px-12", className)}>
       {children}
     </Component>
   );
