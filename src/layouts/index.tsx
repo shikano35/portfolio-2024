9e93a7661e109ab03923771f0d2f4ec00ab0c680
shiitake <pegtar377512@gmail.com>
2025-01-06 12:04:03 +0900

    feat: add Container component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,15 +1,16 @@
 import { Providers } from "@/layouts/Provider";
 import { Footer } from "@/layouts/Footer";
 import { Header } from "@/layouts/Header";
+import { Container } from "@/components/Container";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
-      <div className="relative flex flex-col min-h-screen">
+      <Container className="relative flex flex-col min-h-screen">
         <Header />
         <main className="flex-auto">{children}</main>
         <Footer />
-      </div>
+      </Container>
     </Providers>
   );
 }
