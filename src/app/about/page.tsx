3b456b5741a7355d0542397ff5e1b0c798158e0d
shiitake <pegtar377512@gmail.com>
2025-01-06 22:30:48 +0900

    chore: update a single line

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import { FadeIn } from "@/components/FadeIn";
 import { HeadGroup } from "@/components/HeadGroup";
 import { Heading } from "@/components/Heading";
 import { baseMetadata } from "@/lib/metadata";
@@ -15,6 +16,12 @@ export default function AboutPage() {
     <div>
       <HeadGroup className="mb-16">
         <Heading>About</Heading>
+        <FadeIn>
+          <p className="text-center md:text-start text-muted-foreground text-sm leading-8 md:leading-10">
+            こんにちは。エンジニアです。
+            <br />
+          </p>
+        </FadeIn>
       </HeadGroup>
     </div>
   );
