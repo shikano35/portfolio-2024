7450706d970d167c7e01815e102f88979e796c49
shiitake <pegtar377512@gmail.com>
2025-01-06 12:25:31 +0900

    feat: add Heading and HeadGroup components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { HeadGroup } from "@/components/HeadGroup";
+import { Heading } from "@/components/Heading";
+import React from "react";
+
+export default function BlogsPage() {
+  return (
+    <HeadGroup>
+      <Heading>Contact</Heading>
+    </HeadGroup>
+  );
+}
