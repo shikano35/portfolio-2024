88bcf35994073995708a1e88105b9cee80ff43b1
shikano <pegtar377512@gmail.com>
2025-01-08 22:16:47 +0900

    feat: add template for Log page

---
+++ src/components/Log/Cosense/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Cosense() {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+      <div className="flex flex-col gap-4">
+        <h3 className="text-2xl font-bold">Cosense</h3>
+        <p className="text-muted-foreground">
+          Cosense is a web application that helps you to find the best time to
+          meet with your team members.
+        </p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h3 className="text-2xl font-bold">Cosense</h3>
+        <p className="text-muted-foreground">
+          Cosense is a web application that helps you to find the best time to
+          meet with your team members.
+        </p>
+      </div>
+    </div>
+  );
+}
