bdb201847e9326596fe56262b74426264edc416d
shikano <pegtar377512@gmail.com>
2025-01-10 20:36:59 +0900

    feat: add ZennCard component

--- src/components/Log/Zenn/index.tsx
+++ src/components/Log/Zenn/index.tsx
@@ -4,7 +4,7 @@ import { ZennCard } from "@/components/Card/ZennCard";
 export function Zenn() {
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
         <ZennCard />
       </div>
     </div>
