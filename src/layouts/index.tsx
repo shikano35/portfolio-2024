7ebe5a4b46f90e596d103cf35f9d1f845afbe7eb
shikano <pegtar377512@gmail.com>
2025-01-10 20:29:34 +0900

    feat: remove Container component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,13 @@
 import { Providers } from "@/layouts/Provider";
 import { Footer } from "@/layouts/Footer";
 import { Header } from "@/layouts/Header";
-import { Container } from "@/components/Container";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
-      <Container className="relative flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-auto">{children}</main>
-        <Footer />
-      </Container>
+      <Header />
+      <main className="flex-auto">{children}</main>
+      <Footer />
     </Providers>
   );
 }
