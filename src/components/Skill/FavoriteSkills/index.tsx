e4261252706d81de2686f9dd62a60f8c29796922
shikano <pegtar377512@gmail.com>
2025-01-20 15:04:50 +0900

    feat: Add navigation to skills page

--- src/components/Skill/FavoriteSkills/index.tsx
+++ src/components/Skill/FavoriteSkills/index.tsx
@@ -1,37 +1,51 @@
 import React from "react";
 import Link from "next/link";
-import clsx from "clsx";
+import { cn } from "@/lib/utils";
 import { fetchFavoriteSkills } from "@/components/Skill";
 import { FadeIn } from "@/components/FadeIn";
 import { SkillCard } from "@/components/Card/SkillCard";
+import { ArrowLongRightIcon } from "@heroicons/react/24/solid";
 
 export async function FavoriteSkills({ className = "" }) {
   const favoriteSkills = await fetchFavoriteSkills();
 
   return (
-    <div
-      className={clsx(
-        "grid grid-cols-3 gap-6 justify-items-center md:justify-items-start",
-        className
-      )}
-    >
-      {favoriteSkills.map((skill) => {
-        const Icon = skill.icon;
-        return (
-          <FadeIn key={skill.id}>
-            <Link
-              href={`/skills/${skill.id}`}
-              aria-label={`View details about ${skill.name}`}
-            >
-              <div className="flex flex-col items-center">
-                <SkillCard>
-                  <Icon className="w-12 h-12" />
-                </SkillCard>
-              </div>
-            </Link>
-          </FadeIn>
-        );
-      })}
+    <div className="flex flex-col">
+      <div
+        className={cn(
+          "grid grid-cols-3 gap-6 justify-items-center md:justify-items-start",
+          className
+        )}
+      >
+        {favoriteSkills.map((skill) => {
+          const Icon = skill.icon;
+          return (
+            <FadeIn key={skill.id}>
+              <Link
+                href={`/skills/${skill.id}`}
+                aria-label={`View details about ${skill.name}`}
+              >
+                <div className="flex flex-col items-center">
+                  <SkillCard>
+                    <Icon className="w-12 h-12" />
+                  </SkillCard>
+                </div>
+              </Link>
+            </FadeIn>
+          );
+        })}
+      </div>
+      <FadeIn>
+        <div className="flex justify-end m-5">
+          <Link
+            href="/skills"
+            className="flex items-center text-sm p-1 text-muted-foreground hover:text-highlight transition"
+          >
+            その他のスキルを見る
+            <ArrowLongRightIcon className="w-4 h-4 ml-1" />
+          </Link>
+        </div>
+      </FadeIn>
     </div>
   );
 }
