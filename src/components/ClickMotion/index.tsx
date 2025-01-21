90a3112f0afdcb3139e271162da5bc0ab5db8a40
shikano <pegtar377512@gmail.com>
2025-01-22 00:06:43 +0900

    feat: Add ClickMotion component

---
+++ src/components/ClickMotion/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ComponentPropsWithoutRef } from "react";
+import { motion } from "motion/react";
+import React from "react";
+
+export function ClickMotion(
+  props: ComponentPropsWithoutRef<typeof motion.div> & {
+    smallAnimation?: boolean;
+  }
+) {
+  return (
+    <motion.div
+      whileHover={{
+        scale: props.smallAnimation ? 1 : 1.01,
+        y: props.smallAnimation ? -0.5 : -1.5,
+      }}
+      whileTap={{ scale: 0.99, y: props.smallAnimation ? 0.5 : 1 }}
+      transition={{ duration: 0.1, ease: "easeInOut" }}
+      {...props}
+    />
+  );
+}
