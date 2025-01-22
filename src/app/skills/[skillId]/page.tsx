b22b762c7fdb44a900dcc002b4716ec72ca4807c
shikano <pegtar377512@gmail.com>
2025-01-22 22:06:00 +0900

    feat: Create templates for skill pages

--- src/app/skills/[skillId]/page.tsx
+++ src/app/skills/[skillId]/page.tsx
@@ -1,19 +1,63 @@
-import { HeadGroup } from "@/components/HeadGroup";
-import { Heading } from "@/components/Heading";
-import { SkillList } from "@/components/Skill";
 import React from "react";
+import { Metadata } from "next";
+import { SkillList } from "@/components/Skill";
+import { getSkill, getSkills } from "@/lib/microcms";
+import { Heading } from "@/components/Heading";
+import { FadeIn } from "@/components/FadeIn";
+import { baseMetadata } from "@/lib/metadata";
+import { Container } from "@/components/Container";
+import { HeadGroup } from "@/components/HeadGroup";
 
-export default function SkillsPage() {
+export async function generateMetadata({
+  params,
+}: {
+  params: { skillId: string };
+}): Promise<Metadata> {
+  // paramsを非同期的に解決
+  const { skillId } = await params;
+  const skill = await getSkill(skillId, { fields: "name,description" });
+
+  return {
+    ...baseMetadata,
+    title: skill.name,
+    description: skill.description,
+  };
+}
+
+// Static Paramsの生成
+export async function generateStaticParams() {
+  const skills = await getSkills({ fields: "id" });
+  return skills.map((skill) => ({
+    skillId: skill.id,
+  }));
+}
+
+// ページのメインコンポーネント
+export default async function SkillDetailPage({
+  params,
+}: {
+  params: { skillId: string };
+}) {
+  // params.skillId を直接使用せず変数に代入
+  const { skillId } = await params;
+
+  const skill = await getSkill(skillId);
   return (
-    <div>
-      <HeadGroup className="mb-32">
-        <Heading>Skills</Heading>
-        <p className="text-center md:text-start text-muted-foreground text-base leading-8 md:leading-10">
-          disctiption
-        </p>
+    <Container>
+      <HeadGroup>
+        <Heading>{skill.name}</Heading>
+        <FadeIn>
+          <p className="text-muted-foreground text-base leading-7">
+            {skill.description}
+          </p>
+        </FadeIn>
       </HeadGroup>
-
-      <SkillList />
-    </div>
+      <section className="mt-32">
+        <Heading as="h2" className="mb-8">
+          Skills
+        </Heading>
+        <SkillList className="grid-cols-3 md:grid-cols-5 gap-16 justify-items-center" />
+      </section>
+    </Container>
   );
 }
