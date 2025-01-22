e45a1174ece3c0b12a4bdc64d972848436d94b96
shikano <pegtar377512@gmail.com>
2025-01-22 22:07:28 +0900

    feat: Add option to hide border

--- src/components/Card/SkillCard/index.tsx
+++ src/components/Card/SkillCard/index.tsx
@@ -1,30 +1,32 @@
 import React, { ElementType, ReactNode, ComponentPropsWithoutRef } from "react";
 import { cn } from "@/lib/utils";
-import { ClickMotion } from "@/components/ClickMotion";
 
 type CardProps<T extends ElementType = "div"> = {
   as?: T;
   children: ReactNode;
   className?: string;
+  showBorder?: boolean;
 };
 
 export function SkillCard<T extends ElementType = "div">({
   as,
   children,
   className,
+  showBorder = true,
 }: CardProps<T> & ComponentPropsWithoutRef<T>) {
   const Component = as || "div";
 
   return (
-    <ClickMotion>
-      <Component
-        className={cn(
-          "p-2 border-2 shadow-sm hover:shadow-md border-muted hover:border-border bg-popover rounded-xl transition active:shadow-sm",
-          className
-        )}
-      >
-        {children}
-      </Component>
-    </ClickMotion>
+    <Component
+      className={cn(
+        "p-2 transition",
+        showBorder
+          ? "shadow-sm border-2 border-muted hover:border-border hover:shadow-md bg-popover rounded-xl active:shadow-sm"
+          : "",
+        className
+      )}
+    >
+      {children}
+    </Component>
   );
 }
