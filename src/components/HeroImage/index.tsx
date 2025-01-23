a2cc3d3270aa40b4b3533ea27b5f622bdfea2238
shikano <pegtar377512@gmail.com>
2025-01-24 05:56:32 +0900

    fix: z-index

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -8,7 +8,7 @@ type HeroImageProps = {
 
 export function HeroImage({ children, className }: HeroImageProps) {
   return (
-    <div className="relative -z-50 md:col-span-5 md:flex md:items-center">
+    <div className="relative -z-30 md:col-span-5 md:flex md:items-center">
       <div className={cn("absolute transform -translate-y-1/2", className)}>
         {children}
       </div>
