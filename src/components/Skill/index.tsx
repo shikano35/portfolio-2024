3ff9e194141c9e57e7b50a6b4d24ed99885f1f28
shikano <pegtar377512@gmail.com>
2025-01-23 16:20:29 +0900

    style: Modify styles

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -131,7 +131,7 @@ export async function SkillList({
             <Link
               href={`/skills/${skill.id}`}
               aria-label={`View details about ${skill.name}`}
-              className="rounded-lg"
+              className="rounded-lg text-center"
             >
               {showClickMotion ? (
                 <ClickMotion>
