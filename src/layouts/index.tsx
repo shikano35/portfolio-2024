f7434cd98ffbba8b443d1bb1ac028b046a2b941e
shikano <pegtar377512@gmail.com>
2025-01-24 06:00:01 +0900

    feat: Add Pattern Component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,13 @@
 import { Providers } from "@/layouts/Provider";
 import { Footer } from "@/layouts/Footer";
 import { Header } from "@/layouts/Header";
+import { Pattern } from "@/components/Pattern";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
       <Header />
+      <Pattern />
       <main className="flex-auto overflow-hidden">{children}</main>
       <Footer />
     </Providers>
