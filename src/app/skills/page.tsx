e104b393768e21efa96dc9b9803f8d91f1c4b938
shikano <pegtar377512@gmail.com>
2025-01-23 16:16:05 +0900

    feat: Add StarMessage component

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,6 +1,7 @@
 import { HeadGroup } from "@/components/HeadGroup";
 import { Heading } from "@/components/Heading";
 import { SkillList } from "@/components/Skill";
+import { StarMessage } from "@/components/Stars";
 import { baseMetadata } from "@/lib/metadata";
 import { Metadata } from "next";
 import React from "react";
@@ -16,6 +17,7 @@ export default function SkillsPage() {
     <div>
       <HeadGroup className="mb-16">
         <Heading>Skills</Heading>
+        <StarMessage />
       </HeadGroup>
       <SkillList className="grid-cols-3 md:grid-cols-5 gap-16 mb-16 justify-items-center" />
     </div>
