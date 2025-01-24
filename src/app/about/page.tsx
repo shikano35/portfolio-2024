633e5262f8ff04232e7019c2cc195d77d38c3b79
shikano <pegtar377512@gmail.com>
2025-01-24 21:02:17 +0900

    refactor: Improve structure and enhance accessibility

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
+import React from "react";
+import { Metadata } from "next";
+import { Container } from "@/components/Container";
 import { HeadGroup } from "@/components/HeadGroup";
 import { Heading } from "@/components/Heading";
 import { baseMetadata } from "@/lib/metadata";
-import { Metadata } from "next";
-import React from "react";
-import { Container } from "@/components/Container";
 import { FadeIn } from "@/components/FadeIn";
 import { Border } from "@/components/Border";
 import { GithubCard } from "@/components/Card/GithubCard";
@@ -14,7 +14,7 @@ import { SkillList } from "@/components/Skill";
 export const metadata: Metadata = {
   ...baseMetadata,
   title: "About",
-  description: "自己紹介です。",
+  description: "自己紹介ページです。",
 };
 
 type TimelineItem = {
@@ -39,146 +39,149 @@ const timelineData: TimelineItem[] = [
     id: 5,
     year: "2024",
     month: "6月 - 9月",
-    description:
-      "UnityとC#を使用して、東京ゲームショウに向けたゲーム開発を行う",
+    description: "UnityとC#でゲーム開発を行い東京ゲームショウに出展",
   },
   {
     id: 6,
     year: "2024",
     month: "10月",
-    description:
-      "Web技術への興味をきっかけに、本格的にプログラミングの学習を開始する",
+    description: "Web技術に興味を持ちプログラミングを本格的に学習開始",
   },
 ];
 
 export default function AboutPage() {
   return (
-    <>
-      <Container>
-        <div className="md:grid md:grid-cols-12">
-          <HeadGroup className="relative z-10 md:col-span-12">
-            <Heading>About</Heading>
-            <div className="flex items-center justify-center md:justify-start mb-8">
-              <FadeIn>
-                <Logo
-                  alt="My Logo"
-                  width={96}
-                  height={96}
-                  className="dark:hidden h-12 w-12 md:h-16 md:w-16 border border-border rounded-full"
-                />
-                <Logo
-                  alt="My Logo"
-                  width={96}
-                  height={96}
-                  src="/my-icon-dark.webp"
-                  className="hidden dark:block h-12 w-12 md:h-16 md:w-16 border border-border rounded-full"
-                />
-              </FadeIn>
-              <Heading as="h3" className="tracking-wide m-4 whitespace-nowrap">
-                Shin Takemura
-              </Heading>
-            </div>
+    <Container>
+      <header>
+        <HeadGroup>
+          <Heading>About</Heading>
+          <div className="flex items-center justify-center md:justify-start mb-8">
             <FadeIn>
-              <p className="text-center md:text-start text-muted-foreground text-base leading-8 md:leading-10">
-                エンジニアです。
-                <br />
-                モダンなWeb技術を中心に学んでいます。
-                <br />
-                CI/CDの構築やバックエンド開発も勉強しています。
-                <br />
-                シンプルなデザイン、アニメーションが好きです。
-              </p>
+              <Logo
+                alt="My Logo"
+                width={96}
+                height={96}
+                className="dark:hidden h-12 w-12 md:h-16 md:w-16 border border-border rounded-full"
+              />
+              <Logo
+                alt="My Logo"
+                width={96}
+                height={96}
+                src="/my-icon-dark.webp"
+                className="hidden dark:block h-12 w-12 md:h-16 md:w-16 border border-border rounded-full"
+              />
             </FadeIn>
-          </HeadGroup>
-        </div>
-        <Border className="my-16" />
-        <Heading as="h3" className="my-16">
-          Experience
-        </Heading>
-        <section
-          className="relative h-full ml-32"
-          aria-labelledby="timeline-heading"
-        >
-          <div
-            className="absolute h-full border-l border-border"
-            aria-hidden="true"
-          />
-          <Heading as="h2" className="sr-only">
-            Timeline
-          </Heading>
-          <ol className="grid grid-cols-1 gap-16">
-            {timelineData.map((item, index) => (
-              <li key={index} className="flex items-center">
-                <span
-                  className="absolute z-20 h-2 w-2 rounded-full bg-border -ml-[3.5px] inline-block"
-                  aria-hidden="true"
-                />
-                <time
-                  className="flex items-center justify-center -ml-28 md:-ml-32 font-bold text-4xl primary-foreground opacity-15"
-                  dateTime={item.year}
-                >
-                  <FadeIn>{item.year}</FadeIn>
-                </time>
+            <Heading as="h3" className="tracking-wide m-4 whitespace-nowrap">
+              Shin Takemura
+            </Heading>
+          </div>
+          <FadeIn>
+            <p className="text-center md:text-start text-muted-foreground text-base leading-8 md:leading-10">
+              エンジニアです。
+              <br />
+              モダンなWeb技術を中心に学んでいます。
+              <br />
+              CI/CDの構築やバックエンド開発も勉強しています。
+              <br />
+              シンプルなデザインとシンプルなアニメーションが好きです。
+            </p>
+          </FadeIn>
+        </HeadGroup>
+      </header>
 
-                <div className="flex flex-col ml-12 md:ml-16">
-                  <FadeIn>
-                    <h3 className="text-sm font-semibold text-primary text-left">
-                      {item.month}
-                    </h3>
-                    <p className="text-muted-foreground text-left">
-                      {item.description}
-                    </p>
-                  </FadeIn>
-                </div>
-              </li>
-            ))}
-          </ol>
-        </section>
+      <main>
         <Border className="my-16" />
-        <Heading as="h3" className="my-12">
-          Tech
-        </Heading>
-        <div className="flex flex-1 justify-center">
-          <div className="md:grid md:grid-cols-12 md:mr-8">
-            <div className="md:col-span-5 lg:mx-0">
-              <FadeIn>
-                <Heading as="h6">My Favorite Tech Stack</Heading>
-                <SkillList
-                  className="mt-8 grid grid-cols-3 gap-6 justify-items-center md:justify-items-start"
-                  showName={false}
-                  showStars={false}
-                  iconSize="w-12 h-12"
-                  useFavorite={true}
-                />
-              </FadeIn>
-            </div>
-            <div className="md:flex md:flex-auto md:-ml-8 lg:ml-0 md:col-start-7 lg:col-start-8 mt-32 md:mt-0 ">
-              <GithubCard>
-                <ContributionGraph />
-              </GithubCard>
+        <section aria-labelledby="experience-heading">
+          <Heading as="h3" className="my-16">
+            Experience
+          </Heading>
+          <div className="relative h-full ml-32">
+            <div
+              className="absolute h-full border-l border-border"
+              aria-hidden="true"
+            />
+            <ol className="grid grid-cols-1 gap-16">
+              {timelineData.map((item) => (
+                <li key={item.id} className="flex items-center">
+                  <span
+                    className="absolute z-20 h-2 w-2 rounded-full bg-border -ml-[3.5px] inline-block"
+                    aria-hidden="true"
+                  />
+                  <time
+                    className="flex items-center justify-center -ml-28 md:-ml-32 font-bold text-4xl primary-foreground opacity-15"
+                    dateTime={item.year}
+                  >
+                    <FadeIn>{item.year}</FadeIn>
+                  </time>
+                  <div className="flex flex-col ml-12 md:ml-16">
+                    <FadeIn>
+                      <h3 className="text-sm font-semibold text-primary text-left">
+                        {item.month}
+                      </h3>
+                      <p className="text-muted-foreground text-left">
+                        {item.description}
+                      </p>
+                    </FadeIn>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </section>
+
+        <Border className="my-16" />
+        <section aria-labelledby="tech-heading">
+          <Heading as="h3" className="my-12">
+            Tech
+          </Heading>
+          <div className="flex flex-1 justify-center">
+            <div className="md:grid md:grid-cols-12 md:mr-8">
+              <div className="md:col-span-5 lg:mx-0">
+                <FadeIn>
+                  <Heading as="h6">My Favorite Tech Stack</Heading>
+                  <SkillList
+                    className="mt-8 grid grid-cols-3 gap-6 justify-items-center md:justify-items-start"
+                    showName={false}
+                    showStars={false}
+                    iconSize="w-12 h-12"
+                    useFavorite={true}
+                  />
+                </FadeIn>
+              </div>
+              <div className="md:flex md:flex-auto md:-ml-8 lg:ml-0 md:col-start-7 lg:col-start-8 mt-32 md:mt-0">
+                <GithubCard>
+                  <ContributionGraph />
+                </GithubCard>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+
         <Border className="my-16" />
-        <Heading as="h4" className="mt-16 mb-4">
-          Hobbies
-        </Heading>
-        <FadeIn>
-          <p className="text-center md:text-start text-muted-foreground text-sm leading-6">
-            趣味は読書、プログラミング、勉強です。
-            <br />
-            長期的な休みは、一人旅やキャンプなどのアウトドアも楽しみます。
-          </p>
-        </FadeIn>
-        <Heading as="h4" className="mt-12 mb-4">
-          Curiosity
-        </Heading>
-        <FadeIn>
-          <p className="text-center md:text-start text-muted-foreground text-sm leading-6">
-            知識を得たり、物事を深く考えることに幸せを感じます。
-          </p>
-        </FadeIn>
-      </Container>
-    </>
+        <section aria-labelledby="hobbies-heading">
+          <Heading as="h4" className="mt-16 mb-4">
+            Hobbies
+          </Heading>
+          <FadeIn>
+            <p className="text-center md:text-start text-muted-foreground text-sm leading-6">
+              趣味は読書、プログラミング、勉強です。
+              <br />
+              長期的な休みは、一人旅やキャンプなどのアウトドアも楽しみます。
+            </p>
+          </FadeIn>
+        </section>
+
+        <section aria-labelledby="curiosity-heading">
+          <Heading as="h4" className="mt-12 mb-4">
+            Curiosity
+          </Heading>
+          <FadeIn>
+            <p className="text-center md:text-start text-muted-foreground text-sm leading-6">
+              知識を得たり、物事を深く考えることに幸せを感じます。
+            </p>
+          </FadeIn>
+        </section>
+      </main>
+    </Container>
   );
 }
