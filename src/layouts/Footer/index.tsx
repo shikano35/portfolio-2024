e9bd83f7d4e20e428d1802d69aeca821751f3e96
shikano <pegtar377512@gmail.com>
2025-01-26 01:31:31 +0900

    fix: Refactor Footer component to use Container as footer element for improved semantics

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -6,18 +6,16 @@ import { Container } from "@/components/Container";
 export function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <Container>
-        <Border className="flex flex-col mt-16" />
-        <FadeInWithStagger>
-          <div className="flex justify-center md:justify-start pb-16 pt-8 md:pb-10">
-            <LinksNav activeClassName="font-medium text-primary" />
-          </div>
-          <FadeIn className="mb-12 md:mt-8 text-xs text-muted-foreground text-center">
-            &copy; {currentYear} Shin Takemura
-          </FadeIn>
-        </FadeInWithStagger>
-      </Container>
-    </footer>
+    <Container as="footer">
+      <Border className="flex flex-col mt-16" />
+      <FadeInWithStagger>
+        <div className="flex justify-center md:justify-start pb-16 pt-8 md:pb-10">
+          <LinksNav activeClassName="font-medium text-primary" />
+        </div>
+        <FadeIn className="mb-12 md:mt-8 text-xs text-muted-foreground text-center">
+          &copy; {currentYear} Shin Takemura
+        </FadeIn>
+      </FadeInWithStagger>
+    </Container>
   );
 }
