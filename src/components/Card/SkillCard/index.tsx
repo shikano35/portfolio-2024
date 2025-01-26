c555b50eaf60012a23f305a683ed974eb23c3546
shikano <pegtar377512@gmail.com>
2025-01-26 19:48:22 +0900

    fix: Improve transition effect in SkillCard component for smoother UI experience

--- src/components/Card/SkillCard/index.tsx
+++ src/components/Card/SkillCard/index.tsx
@@ -21,7 +21,7 @@ export function SkillCard<T extends ElementType = "div">({
       className={cn(
         "p-2 transition",
         showBorder
-          ? "shadow-sm border-2 border-muted hover:border-border hover:shadow-md bg-popover rounded-xl active:shadow-sm"
+          ? "transition-shadow shadow-sm border-2 border-muted hover:border-border hover:shadow-md bg-popover rounded-xl active:shadow-sm"
           : "",
         className
       )}
