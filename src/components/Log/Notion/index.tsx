f2c2f26de564f09a04691c8ef4f59093763bd727
shikano <pegtar377512@gmail.com>
2025-01-28 01:15:19 +0900

    fix: Notionコンポーネントのコードスタイルを整形

--- src/components/Log/Notion/index.tsx
+++ src/components/Log/Notion/index.tsx
@@ -3,11 +3,8 @@ import { fetchNotionArticles } from "@/lib/notion";
 import NotionCard from "@/components/Card/NotionCard";
 import { FadeIn } from "@/components/FadeIn";
 
-export const revalidate = 86400;
-
 export default async function Notion() {
   const articles = await fetchNotionArticles();
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 py-4">
       {articles.map((article) => (
@@ -18,3 +15,5 @@ export default async function Notion() {
     </div>
   );
 }
+
+export const revalidate = 86400;
