9f10fe43c5ef759bca137319d17f4beeeb1bb01f
shikano <pegtar377512@gmail.com>
2025-01-28 09:52:22 +0900

    refactor: Patternコンポーネントをコメントアウトし、Backgroundコンポーネントを追加

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,16 @@
+import React from "react";
 import { Providers } from "@/layouts/Provider";
 import { Footer } from "@/layouts/Footer";
 import { Header } from "@/layouts/Header";
-import { Pattern } from "@/components/Pattern";
+// import { Pattern } from "@/components/Pattern";
+import { Background } from "@/components/Background";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
       <Header />
-      <Pattern />
+      {/* <Pattern /> */}
+      <Background />
       <main className="flex-auto overflow-hidden">{children}</main>
       <Footer />
     </Providers>
