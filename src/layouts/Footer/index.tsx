0b0e555bc0656879161d3ab35f7b2d9f3cab3a7a
shikano <pegtar377512@gmail.com>
2025-01-29 18:00:24 +0900

    fix: LinksNavコンポーネントからactiveClassNameを削除

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -11,7 +11,7 @@ export function Footer() {
       <Border className="flex flex-col mt-16" />
       <FadeInWithStagger>
         <div className="flex justify-center md:justify-start pb-16 pt-8 md:pb-10">
-          <LinksNav activeClassName="font-medium text-primary" />
+          <LinksNav />
         </div>
         <FadeIn className="mb-12 md:mt-8 text-xs text-muted-foreground text-center">
           &copy; {currentYear} Shin Takemura
