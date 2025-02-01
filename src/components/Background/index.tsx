5f2c6b1907bdb664255fe5b4ae89d6bfd3fb2f88
shikano <pegtar377512@gmail.com>
2025-02-01 20:14:10 +0900

    fix: レイアウトとスタイルを改善し、コンポーネントの一貫性を向上 (#64)

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -5,11 +5,12 @@ export function Background() {
   return (
     <Image
       priority={true}
+      draggable={false}
       src="/pattern.png"
       alt="Pattern"
       layout="fill"
       style={{ objectFit: "cover" }}
-      className="absolute inset-x-0 -top-14 -z-40 blur-3xl opacity-25 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)] dark:[mask-image:linear-gradient(to_bottom_left,white_10%,transparent_70%)]"
+      className="select-none pointer-events-none absolute inset-x-0 -top-14 -z-40 blur-3xl opacity-25 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)] dark:[mask-image:linear-gradient(to_bottom_left,white_10%,transparent_70%)]"
     />
   );
 }
