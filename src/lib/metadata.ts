55703b91c12da6f4438ac8e2ef93a3841ea91cbe
shikano <pegtar377512@gmail.com>
2025-03-01 22:54:25 +0900

    feat: Linkコンポーネントを追加し、外部リンクを追加

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,22 +1,20 @@
 import { Metadata } from "next";
 
-const url = "https://example.com";
-
 export const baseMetadata: Metadata = {
   title: "Shikano's Portfolio",
   description: "This is Shikano's portfolio.",
-  authors: [{ name: "Shikano", url: url }],
+  authors: [{ name: "Shikano", url: "https://shikano35.me" }],
   applicationName: "Shikano's Portfolio",
   openGraph: {
     title: "Shikano's Portfolio",
     description: "This is Shikano's portfolio.",
     type: "website",
-    url: url,
+    url: "https://shikano35.me",
     siteName: "Shikano's Portfolio",
     locale: "ja_JP",
     images: [
       {
-        url: url,
+        url: "https://shikano35.me",
         width: 1200,
         height: 630,
         alt: "Shikano's Portfolio",
