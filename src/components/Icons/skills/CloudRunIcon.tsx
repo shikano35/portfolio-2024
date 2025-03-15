fb1bc22ff45dc89675edf08f4970118e14539235
shikano <pegtar377512@gmail.com>
2025-03-16 01:22:30 +0900

    feat: 新しいアイコンを追加

---
+++ src/components/Icons/skills/CloudRunIcon.tsx
@@ -0,0 +1,36 @@
+import type React from "react";
+
+export function CloudRunIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <title>Icon_24px_CloudRun_Color</title>
+      <defs>
+        <style>{`
+          .cls-1 { fill: #aecbfa; }
+          .cls-1, .cls-2 { fill-rule: evenodd; }
+          .cls-2 { fill: #4285f4; }
+        `}</style>
+      </defs>
+      <g data-name="Product Icons">
+        <g>
+          <polygon
+            className="cls-1"
+            points="8.9 2.63 12.02 12 21.38 12 8.9 2.63"
+          />
+          <polygon
+            className="cls-2"
+            points="21.38 12 12.02 12 8.9 21.38 21.38 12"
+          />
+          <polygon
+            className="cls-2"
+            points="3.44 21.38 6.57 19.81 8.9 12 5.78 12 3.44 21.38"
+          />
+          <polygon
+            className="cls-1"
+            points="3.44 2.63 5.78 12 8.9 12 6.57 4.19 3.44 2.63"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
--- src/components/Icons/skills/FlutterIcon.tsx
+++ src/components/Icons/skills/FlutterIcon.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import type React from "react";
 
 export function FlutterIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 128 128" {...props}>
+      <title>{"Flutter"}</title>
       <g fill="#3FB6D3">
-        <path d="M12.3 64.2L76.3 0h39.4L32.1 83.6zM76.3 128h39.4L81.6 93.9l34.1-34.8H76.3L42.2 93.5z"></path>
+        <path d="M12.3 64.2L76.3 0h39.4L32.1 83.6zM76.3 128h39.4L81.6 93.9l34.1-34.8H76.3L42.2 93.5z" />
       </g>
-      <path fill="#27AACD" d="M81.6 93.9l-20-20-19.4 19.6 19.4 19.6z"></path>
-      <path fill="#19599A" d="M115.7 128L81.6 93.9l-20 19.2L76.3 128z"></path>
+      <path fill="#27AACD" d="M81.6 93.9l-20-20-19.4 19.6 19.4 19.6z" />
+      <path fill="#19599A" d="M115.7 128L81.6 93.9l-20 19.2L76.3 128z" />
       <linearGradient
         id="flutter-original-a"
         gradientUnits="userSpaceOnUse"
@@ -16,14 +17,14 @@ export function FlutterIcon(props: React.SVGProps<SVGSVGElement>) {
         x2="86.825"
         y2="99.399"
       >
-        <stop offset="0" stopColor="#1b4e94"></stop>
-        <stop offset=".63" stopColor="#1a5497"></stop>
-        <stop offset="1" stopColor="#195a9b"></stop>
+        <stop offset="0" stopColor="#1b4e94" />
+        <stop offset=".63" stopColor="#1a5497" />
+        <stop offset="1" stopColor="#195a9b" />
       </linearGradient>
       <path
         fill="url(#flutter-original-a)"
         d="M61.6 113.1l30.8-8.4-10.8-10.8z"
-      ></path>
+      />
     </svg>
   );
 }
